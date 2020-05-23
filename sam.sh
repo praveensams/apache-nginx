024b1d93c32ba7cd49776772d86845ede2a1ff0d
praveensams <praveensams16@gmail.com>
2020-05-23 23:22:28 +0300

    Update sam.sh

--- sam.sh
+++ sam.sh
@@ -1,4 +1,6 @@
-for i in $(seq 10)
-    do
-        echo $i >> /tmp/sam1
-    done
+for i in git ansible 
+do
+yum install $i -y
+done
+
+anisble-pull -U https://github.com/praveensams/apache-nginx.git -i localhost wrap.yaml
